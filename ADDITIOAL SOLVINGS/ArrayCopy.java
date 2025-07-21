2e176f20e261526ef74f364ae2e15209c9e80426
JAYMITHRAN <r.jaymithran1036@gmail.com>
2025-07-21 10:20:49 +0530

    last update

--- ADDITIOAL SOLVINGS/ArrayCopy.java
+++ ADDITIOAL SOLVINGS/ArrayCopy.java
@@ -8,6 +8,11 @@ class Friday{
    int [] modarr = new int[arr.length];
 
    System.arraycopy(arr,0,coparr,0,arr.length);
-   for(int )
+   for(int i=0;i<arr.length;i++ )
+   {
+    modarr[i] = arr[i];
+   }
+   System.out.println("By CopyMethod : "+Arrays.toString(coparr));
+   System.out.println("By ManualMethod : "+Arrays.toString(modarr));
   }
 }
